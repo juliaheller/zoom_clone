41caea2ef3e0307f887daa867f20d7ed5bb73434
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-07-27 12:24:25 +0200

    Add script and set up socket

--- server.js
+++ server.js
@@ -13,4 +13,12 @@ app.get("/", (req, res) => {
 app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room });
 });
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+        console.log(roomId, userId);
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+    });
+});
+
 server.listen(3000);
